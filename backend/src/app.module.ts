901417cf3a944dd36c7d6fd8cb0b69cb3a5384a0
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-05-31 19:55:55 +0300

    Prepared the project template

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { GoalsModule } from './goals/goals.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [UserModule, GoalsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
