03d46b80adf6a59a0f8bde8d2aedce3b27823479
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-06-09 22:35:17 +0300

    setting up DB and swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('User example')
+    .setDescription('The User API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+
+  const userDocument = SwaggerModule.createDocument(app, options, {
+    include: [UserModule],
+  });
+
+  SwaggerModule.setup('api/user', app, userDocument);
+
   await app.listen(3000);
 }
 bootstrap();
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Entity, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity({ name: 'users' })
+export class User {
+  @ApiProperty()
+  @Column('uuid')
+  id: string;
+
+  @Column()
+  @ApiProperty()
+  username: string;
+
+  @Column()
+  @ApiProperty()
+  password: string;
+
+  @Column('json')
+  @ApiProperty()
+  roles: JSON;
+}
