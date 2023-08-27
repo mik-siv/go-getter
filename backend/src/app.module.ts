2d792997c5d0a7699b430bca56bf9fb2f51da051
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-08-27 22:25:03 +0300

    chore(project): schema validation for config module, tests moved
    installed Joi
    configmodule now validates the env variables
    moved auth specs to a separate folder

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,12 +7,21 @@ import { GoalsModule } from './goals/goals.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { dataSourceOptions } from './db/data-source';
+import * as Joi from 'joi'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `${process.env.NODE_ENV}.env`,
+      validationSchema: Joi.object({
+        PGHOST: Joi.string().required(),
+        PGDATABASE: Joi.string().required(),
+        PGUSER: Joi.string().required(),
+        PGPASSWORD: Joi.string().required(),
+        PGPORT: Joi.string().required(),
+        JWTSECRET: Joi.string().required()
+      }).required()
     }),
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
