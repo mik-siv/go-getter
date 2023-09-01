8e62af559d971092812bbb184d7845d00b93250c
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-09-01 22:26:38 +0300

    refactor(test): refactored the repository mock solution to be generic
    created a backend/src/mocks/generic.repository.fake.ts - shared mock repository
    backend/src/mocks/user.repository.fake.ts now extends a shared mock repository

--- backend/src/mocks/user.repository.fake.ts
+++ backend/src/mocks/user.repository.fake.ts
@@ -1,28 +1,4 @@
 import { User } from '../user/entities/user.entity';
+import { GenericRepositoryFake } from './generic.repository.fake'
 
-export class UserRepositoryFake {
-  private users: User[] = [];
-
-  async create(userData: { [key: string]: any }): Promise<User> {
-    const user = new User();
-    user.id = userData.id;
-    user.username = userData.username;
-    user.email = userData.email;
-    user.password = userData.password;
-    return Promise.resolve(user);
-  }
-
-  async save(user: User): Promise<User> {
-    this.users.push(user);
-    return Promise.resolve(user);
-  }
-
-  async findOne(conditions: any): Promise<User | undefined> {
-    const user = this.users.find((user) => {
-      return Object.entries(conditions.where).every(
-        ([key, value]) => user[key] === value,
-      );
-    });
-    return Promise.resolve(user);
-  }
-}
+export class UserRepositoryFake extends GenericRepositoryFake<User> { }
