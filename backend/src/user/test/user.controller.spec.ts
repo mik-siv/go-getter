6bc61bfb5bb0a7e8ab0ae021dfefe7c2399c686e
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-09-02 19:12:56 +0300

    test(user-controller): updated the user.controller.spec.ts with new tests
    user.controller.spec.ts - new tests: user registration green path, duplicate user registration.

--- backend/src/user/test/user.controller.spec.ts
+++ backend/src/user/test/user.controller.spec.ts
@@ -2,10 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
 import { UserRepositoryFake } from '../../mocks/user.repository.fake';
+import { ConflictException } from '@nestjs/common';
 
 describe('UserController', () => {
   let controller: UserController;
   let repository: UserRepositoryFake;
+  let service: UserService;
+  let serviceCreate;
+  let userData;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +18,36 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+    serviceCreate = jest.spyOn(service, 'create');
+    userData = {
+      username: 'Test',
+      password: 'somepassword',
+      email: 'test@test.com',
+    };
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should register a user', async () => {
+    await controller.create(userData);
+    expect(serviceCreate).toBeCalledTimes(1);
+    expect(serviceCreate).toBeCalledWith(userData);
+  })
+
+  it('duplicate user registration', async () => {
+    serviceCreate.mockImplementation(() => {
+      throw new ConflictException('User Already Exists');
+    })
+    const expectedErrorResponse = {
+      error: 'Conflict',
+      message: 'User Already Exists',
+      statusCode: 409,
+    };
+    const errorResponse = await controller.create(userData);
+    expect(errorResponse).toEqual(expectedErrorResponse);
+  })
+
 });
