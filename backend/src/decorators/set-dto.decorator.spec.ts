9ca6eb52b93d32e638ff76533939f9533a8c0bca
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-09-21 23:37:43 +0300

    test(auth): set-dto.decorator.spec.ts covered, local-auth.guard.spec.ts coverage in progress
    new file:   backend/src/auth/guards/local-auth.guard.spec.ts
    new file:   backend/src/decorators/set-dto.decorator.spec.ts

---
+++ backend/src/decorators/set-dto.decorator.spec.ts
@@ -0,0 +1,15 @@
+import { RouteDto, DTO_KEY } from "./set-dto.decorator";
+
+describe('SetDtoDecorator', () => {
+    it('should create a decorator', () => {
+        const decorator = RouteDto({} as any);
+        expect(decorator).toBeDefined();
+    });
+
+    it('should set metadata', () => {
+        const route = {} as any
+        RouteDto({} as any)(route);
+        const metadata = Reflect.getMetadata(DTO_KEY, route);
+        expect(metadata).toBeDefined();
+    });
+});
