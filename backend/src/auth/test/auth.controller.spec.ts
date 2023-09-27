7b4103c5c0f81d90b9a6d659cf78ef81811f30f4
Sivinskikh Mikhail <mikhail.sivinskikh@gmail.com>
2023-09-27 21:34:10 +0300

    test(auth): new spec for auth.controller
    auth.controller.spec.ts - new test to validate the login method

--- backend/src/auth/test/auth.controller.spec.ts
+++ backend/src/auth/test/auth.controller.spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
-import { UserService } from '../../user/user.service';
-import { JwtService } from '@nestjs/jwt';
-import { UserRepositoryFake } from '../../mocks/user.repository.fake';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let fakeAuthService = {
+    login: jest.fn()
+  }
+  let fakeRequest = {
+    body: {
+      user: {
+        email: 'test@test.com',
+        password: 'test'
+      }
+    }
+  }
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, UserService, JwtService, { provide: 'UserRepository', useValue: UserRepositoryFake }]
+      providers: [{ provide: AuthService, useValue: fakeAuthService },]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -22,6 +31,11 @@ describe('AuthController', () => {
   });
 
   it('should login a user', async () => {
-
+    let fakeAuthTokenData = { access_token: 'fake-access-token' }
+    fakeAuthService.login.mockReturnValue(fakeAuthTokenData);
+    const result = await controller.login(fakeRequest.body);
+    expect(result).toEqual(fakeAuthTokenData);
+    expect(fakeAuthService.login).toHaveBeenCalledWith(fakeRequest.body.user); // No need to access body.user here
+    expect(fakeAuthService.login).toHaveBeenCalledTimes(1);
   })
 });
