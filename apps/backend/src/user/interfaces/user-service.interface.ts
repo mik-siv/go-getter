f826f2b82af04e6af86c2a5504c17cc2a76f4a70
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-01-07 12:40:56 +0200

    chore(lint): added lint comment for the empty user service interface

--- apps/backend/src/user/interfaces/user-service.interface.ts
+++ apps/backend/src/user/interfaces/user-service.interface.ts
@@ -3,5 +3,6 @@ import { UpdateUserDto } from '../dto/update-user.dto';
 import { User } from '../entities/user.entity';
 import { ICrudService } from '../../common/types/interfaces/crud.interface';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IUserService extends ICrudService<User, CreateUserDto, UpdateUserDto> {
 }
