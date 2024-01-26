76c3f2a00de52490af7807f184502ec30dbcb299
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-01-26 21:02:36 +0200

    feat(frontend): Enhance login page functionality and validation
    Introduced new functionalities to the login page including confirm password and error states. Refactored the handleLogin and handleRegister functions to provide validation for form fields and error handling. A switch between login and register modes was also added improving the user experience.

--- apps/frontend/src/pages/login.page.tsx
+++ apps/frontend/src/pages/login.page.tsx
@@ -4,11 +4,14 @@ import { AuthContext } from '../contexts/authentication.context.ts';
 
 const LoginPage = () => {
   const { logIn } = useContext(AuthContext)!;
+  const [isLoginMode, setIsLoginMode] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfrimPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const handleLogin = async () => {
     const response = await fetch('/api/auth/login', {
       method: 'POST',
       headers: {
@@ -17,17 +20,50 @@ const LoginPage = () => {
       body: JSON.stringify({ email, password }),
     });
 
-    const data: { access_token: string } = await response.json();
+    const data: { access_token: string, message?: string } = await response.json();
+    if (!response.ok && data.message) return setError(data.message ?? 'Server error, try again later');
     if (data.access_token) {
       logIn(data.access_token);
     }
+
     setPassword('');
     setEmail('');
   };
 
-  const handleRegister = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleRegister = async () => {
+    if (!password || !email || !confirmPassword || !username) return setError('Please fill in all the fields');
+    if (password !== confirmPassword) return setError('Passwords don\'t match');
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password, username }),
+    });
+
+
+    const data = await response.json();
+    if (!response.ok) return setError(data.message);
+    if (response.ok) {
+      setIsLoginMode(true);
+    }
+
+    setPassword('');
+    setUsername('');
+    setConfrimPassword('');
+  };
+
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log('Register button clicked. Email: ', email, ' Password: ', password);
+    setError('');
+
+    if (isLoginMode && e.currentTarget.name === 'login') {
+      handleLogin();
+    } else if (!isLoginMode && e.currentTarget.name === 'register') {
+      handleRegister();
+    } else {
+      setIsLoginMode(!isLoginMode);
+    }
   };
 
   return (
@@ -39,7 +75,7 @@ const LoginPage = () => {
       </div>
       <div className="form">
         <div className="control">
-          <h2>Login</h2>
+          <h2>{isLoginMode ? 'Login' : 'Register'}</h2>
           <form>
             <input
               className="input-field"
@@ -57,9 +93,42 @@ const LoginPage = () => {
               required
               placeholder="Password"
             />
+            {!isLoginMode && (
+              <>
+                <input
+                  className="input-field"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfrimPassword(e.target.value)}
+                  required
+                  placeholder="Confirm Password"
+                />
+                <input
+                  className="input-field"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  placeholder="Your Name"
+                />
+              </>
+            )}
+            {error && <div className="error">{error}</div>}
             <div>
-              <button className="btn" onClick={handleLogin}>Login</button>
-              <button className="btn" onClick={handleRegister}>Register</button>
+              <button
+                className="btn"
+                name="login"
+                onClick={handleButtonClick}
+              >
+                Login
+              </button>
+              <button
+                className="btn"
+                name="register"
+                onClick={handleButtonClick}
+              >
+                Register
+              </button>
             </div>
           </form>
         </div>
