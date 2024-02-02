c4ba95b333683497d6e54acad7d1d2dbb96f89a0
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-02-02 22:49:15 +0200

    documentation: added JSDocs to roles.guard.ts and role.decorator.ts

--- apps/backend/src/user/decorators/role.decorator.ts
+++ apps/backend/src/user/decorators/role.decorator.ts
@@ -1,4 +1,11 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '../entities/user-roles.enum';
 
-export const Roles = (...roles: UserRole[]) => SetMetadata(roles, roles);
+export const ROLES_KEY = 'roles'
+
+/**
+ * Sets the metadata for the specified roles.
+ *
+ * @param {...UserRole[]} roles - The user roles to be set as metadata.
+ */
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
--- apps/backend/src/user/guards/roles.guard.ts
+++ apps/backend/src/user/guards/roles.guard.ts
@@ -1,13 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from '../entities/user-roles.enum';
+import { ROLES_KEY } from '../decorators/role.decorator';
 
+/**
+ * A guard that checks if a user has the required roles to access a route.
+ * It implements the CanActivate interface from the Nest.js framework.
+ */
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
+  /**
+   * Determines if the user is authorized to access the given endpoint by the RBAC.
+   *
+   * @param {ExecutionContext} context - The execution context of the current request.
+   * @returns {boolean} - Returns true if the user is authorized, otherwise false.
+   */
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>('roles', [
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
