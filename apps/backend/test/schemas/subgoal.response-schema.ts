9eb3270e021083147f178ff5f44790efb515cc7d
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-02-05 21:50:05 +0200

    Refactor goal and subgoal response schemas
    Removed userResponseSchema and goalResponseSchema imports from goal and subgoal schemas. Replaced their instances with plain Joi objects. Additionally, made 'description' field in the metadata a required parameter. These changes simplify validation and ensure each goal and subgoal must have a description.

--- apps/backend/test/schemas/subgoal.response-schema.ts
+++ apps/backend/test/schemas/subgoal.response-schema.ts
@@ -1,6 +1,4 @@
 import * as Joi from 'Joi';
-import { userResponseSchema } from './user.response-schema';
-import { goalResponseSchema } from './goal.response-schema';
 
 /**
  * Represents the subgoal response schema.
@@ -10,12 +8,12 @@ export const subgoalResponseSchema = (): Joi.ObjectSchema => {
   return Joi.object({
     id: Joi.string().guid().required(),
     name: Joi.string().required(),
+    created_by: Joi.object().allow(null),
+    goal_subgoals: Joi.array().allow(null),
     private: Joi.boolean().required(),
     parent: Joi.any(),
     metadata: Joi.object({
-      description: Joi.string().required(),
+      description: Joi.string().allow('').required(),
     }).required(),
-    created_by: userResponseSchema(),
-    goal_subgoals: Joi.array().items(goalResponseSchema()).allow(null),
   });
 };
