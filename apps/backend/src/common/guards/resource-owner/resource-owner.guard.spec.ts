649d1e087a54dc7e937f297051d9367fd10ed91d
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-04-14 12:10:37 +0300

    test: updated the spec for resource-owner.guard.spec.ts to match the changes

--- apps/backend/src/common/guards/resource-owner/resource-owner.guard.spec.ts
+++ apps/backend/src/common/guards/resource-owner/resource-owner.guard.spec.ts
@@ -1,32 +1,41 @@
-import { ResourceOwnerGuard } from './resource-owner.guard';
-import { UserJwtData } from '../../types/general.types';
 import { BadRequestException, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { OwnedResource } from '../../constants/enums/owned-resources.enum';
 import { RolesGuard } from '../roles/roles.guard';
+import { UserJwtData } from '../../types/general.types';
+import { ResourceOwnerGuard } from './resource-owner.guard';
+import { UserRole } from '../../../user/entities/user-roles.enum';
 
 jest.mock('../roles/roles.guard');
-describe('ResourceOwner guard', () => {
+jest.mock('../../../user/user.service');
+
+describe('ResourceOwnerGuard', () => {
   let guard: ResourceOwnerGuard;
-  const reflectorMock = { getAllAndOverride: jest.fn() };
+  const reflectorMock = {
+    getAllAndOverride: jest.fn().mockReturnValue([]),
+    getHandler: jest.fn(),
+    getClass: jest.fn(),
+  };
   let mockExecutionContext: ExecutionContext;
   const user: UserJwtData = {
     id: '123',
     goals: ['123', '456'],
     subgoals: ['789', '101112'],
     contributing_to: ['1', '2'],
-    username: 'test-user',
-    roles: [],
+    username: 'test_user',
+    roles: [UserRole.USER],
   };
   const fakeUserService = {
-    validateUserResourceAllowance: jest.fn(),
+    validateUserResourceAllowance: jest.fn().mockReturnValue(true),
   };
 
   beforeEach(() => {
     guard = new ResourceOwnerGuard(reflectorMock as any, fakeUserService as any);
     mockExecutionContext = {
-      switchToHttp: jest.fn().mockReturnValue({ getRequest: jest.fn() }),
       getHandler: jest.fn(),
       getClass: jest.fn(),
+      switchToHttp: jest.fn().mockReturnValue({
+        getRequest: jest.fn().mockReturnValue({ user, params: { id: '123' } }),
+      }),
     } as any as ExecutionContext;
   });
 
@@ -41,52 +50,47 @@ describe('ResourceOwner guard', () => {
     ['contributing_to', '123', false],
     ['id', '123', true],
     ['id', '456', false],
-  ])('should return %s when checking ownership of %s', (resource, resourceId, expected) => {
+  ])('should return %s when checking ownership of %s', async (resource, resourceId, expected) => {
     const checkFn = guard['checkResourceOwnership'](user, resourceId);
-    const result = checkFn(resource as any);
-    expect(result).toBe(expected);
+    expect(await checkFn(resource as any)).toEqual(expected);
   });
 
-  it('should throw BadRequestException for invalid resource', () => {
+  it('should throw BadRequestException for invalid resource', async () => {
     const checkFn = guard['checkResourceOwnership'](user, '123');
-    expect(() => checkFn('invalid' as any)).toThrow(BadRequestException);
+    try {
+      checkFn('invalid' as any);
+    } catch (e) {
+      expect(e).toBeInstanceOf(BadRequestException);
+    }
   });
 
-  it('should allow route activation based on user', () => {
-    reflectorMock.getAllAndOverride.mockReturnValue([]);
-    const canActivateResult = guard.canActivate(mockExecutionContext);
+  it('should allow route activation based on user', async () => {
+    const canActivateResult = await guard.canActivate(mockExecutionContext);
     expect(canActivateResult).toBe(true);
   });
 
-  it('should throw an error if user is undefined', () => {
+  it('should throw an error if user is undefined', async () => {
     reflectorMock.getAllAndOverride.mockReturnValue([OwnedResource.USER_ID]);
     (mockExecutionContext.switchToHttp().getRequest as jest.Mock).mockReturnValue({ user: undefined });
-    expect(() => guard.canActivate(mockExecutionContext)).toThrow(new UnauthorizedException());
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(UnauthorizedException);
   });
 
-  it('should allow access if resource is not requested by id', () => {
+  it('should allow access if resource is not requested by id', async () => {
     reflectorMock.getAllAndOverride.mockReturnValue([OwnedResource.USER_ID]);
     (mockExecutionContext.switchToHttp().getRequest as jest.Mock).mockReturnValue({ user, params: { id: undefined } });
-    const canActivateResult = guard.canActivate(mockExecutionContext);
+    const canActivateResult = await guard.canActivate(mockExecutionContext);
     expect(canActivateResult).toBe(true);
   });
 
-  it('should allow access based on owned resources using checkResourceOwnership', () => {
+  it('should allow access based on owned resources using checkResourceOwnership', async () => {
     reflectorMock.getAllAndOverride.mockReturnValue([OwnedResource.USER_ID, OwnedResource.CONTRIBUTING_TO]);
-    (mockExecutionContext.switchToHttp().getRequest as jest.Mock).mockReturnValue({ user, params: { id: '123' } });
-    const canActivateResult = guard.canActivate(mockExecutionContext);
+    const canActivateResult = await guard.canActivate(mockExecutionContext);
     expect(canActivateResult).toBe(true);
   });
 
-  it('should allow access to an admin user', () => {
-    reflectorMock.getAllAndOverride.mockReturnValue([
-      OwnedResource.USER_ID,
-      OwnedResource.CONTRIBUTING_TO,
-      OwnedResource.SUBGOALS,
-      OwnedResource.GOALS,
-    ]);
-    (mockExecutionContext.switchToHttp().getRequest as jest.Mock).mockReturnValue({ user, params: { id: '123' } });
+  it('should allow access to an admin user', async () => {
     RolesGuard.checkUserPermissionForRoles = jest.fn().mockReturnValue(true);
-    expect(guard.canActivate(mockExecutionContext)).toEqual(true);
+    const canActivateResult = await guard.canActivate(mockExecutionContext);
+    expect(canActivateResult).toBe(true);
   });
 });
