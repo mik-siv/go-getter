daa1f638ff08f997a9b24ec505b56a54be79180c
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-04-15 22:30:16 +0300

    feat: Create MaterialModule for Angular Material imports
    This commit introduces a new MaterialModule to consolidate Angular Material component imports. The module currently only includes MatToolbarModule, but it is designed to easily accommodate additional imports as needed. It ensures cleaner and more organized code structure.

---
+++ apps/frontend_angular/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const material = [MatToolbarModule];
+
+@NgModule({
+  imports: [material],
+  exports: [material],
+})
+export class MaterialModule {
+}
