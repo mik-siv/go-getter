cd57f47d09c8dd59c6566ed06360b36690df3c44
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-04-16 20:37:31 +0300

    feat: enhance layout formatting
    Improved layout formatting with CSS enhancements. These changes introduce a cleaner, more manageable code structure. Also, a new fixed-header-height variable has been added for layout improvements.

--- apps/frontend_angular/src/app/home/home.component.ts
+++ apps/frontend_angular/src/app/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  goalsApiData = [{
+    id: '1',
+    name: 'first goal',
+    subgoals: [{ name: '1st subgoal' }, { name: 'second subgoal' }],
+  }, {
+    id: '2',
+    name: 'Become a Backend developer',
+    subgoals: [{ name: '3rd subgoal' }, { name: '4th subgoal' }],
+  }];
 }
--- apps/frontend_angular/src/app/material/material.module.ts
+++ apps/frontend_angular/src/app/material/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
-const material = [MatToolbarModule];
+const material = [MatToolbarModule, MatSidenavModule];
 
 @NgModule({
   imports: [material],
