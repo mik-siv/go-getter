9f964cd97e2984468c54db5fe42cd8996c50c65c
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-04-29 20:35:06 +0300

    feat: beautify subgoal cards UI and enhance details
    The updates include implementing a Grid List to display the SubgoalCard components and introducing new CSS rules to improve the look and feel. Simultaneously, adjustments have been made in the Goal service to ensure the compatibility of lengthier subgoal details with the enhanced UI. Import of MatGridListModule was also added to the MaterialModule.

--- apps/frontend_angular/src/app/material/material.module.ts
+++ apps/frontend_angular/src/app/material/material.module.ts
@@ -4,8 +4,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule, MatListOption, MatSelectionList } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
-const material = [MatToolbarModule, MatSidenavModule, MatListModule, MatListOption, MatSelectionList, MatCardModule, MatButtonModule];
+const material = [MatToolbarModule, MatSidenavModule, MatListModule, MatListOption, MatSelectionList, MatCardModule, MatButtonModule, MatGridListModule];
+
 
 @NgModule({
   imports: [material],
