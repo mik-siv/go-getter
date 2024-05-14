b1d584690b893c2b84dfab4e809e982ee04afeca
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-05-14 18:44:55 +0300

    refactor: removes react frontend folder

--- apps/frontend/src/app/app.component.spec.ts
+++ apps/frontend/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'frontend_angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend_angular');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend_angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
