7f1e2b6b0eb7db6c68ca79881d0960c1bca231e0
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-05-15 20:27:46 +0300

    Update Angular dependencies in frontend application
    The Angular dependencies in the frontend application have been updated to their latest versions. These changes ensure that the application uses the most recent and secure Angular releases. The dependencies were updated in both the package-lock.json file and the apps/frontend node.

--- apps/frontend/src/app/shared/services/data-access/crud.service.spec.ts
+++ apps/frontend/src/app/shared/services/data-access/crud.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { CrudService } from './crud.service';
 
 describe('CrudService', () => {
-  let service: CrudService;
+  let service: CrudService<string>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
