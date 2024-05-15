43093a9322fcc91ec6add1e3282509e7ca56b626
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-05-15 20:22:45 +0300

    feat: Add new LayoutComponent to manage site layout
    A new LayoutComponent has been added that encapsulates the entire content of the site except for the header. The component adjusts the site content according to header height, ensuring uniform layout across different pages. The relevant changes include modifications to app.component.html where LayoutComponent is included and removal of margin-top and height correction in home.component.scss.

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
