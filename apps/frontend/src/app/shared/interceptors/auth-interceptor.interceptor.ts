a451ae829752a6bbbb48c12aba94c1035939b3a3
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-06-16 21:38:29 +0300

    feat: Add AuthInterceptor and its test file
    A new `AuthInterceptor` is created to add an Authorization header to all HTTP requests if a token is present. Meanwhile, a test file is provided to ensure its proper implementation and functionality.

---
+++ apps/frontend/src/app/shared/interceptors/auth-interceptor.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/data-access/auth/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.authService.token();
+    if (token) {
+      const authReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
+  }
+}
