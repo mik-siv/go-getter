29a856002710785cb262eca66f7937654ff02874
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-06-16 21:38:02 +0300

    feat: Add new modules to material module
    In this commit, new material modules including MatFormFieldModule, MatIconModule, and MatInputModule have been imported and added to the Material module in the frontend app. This will allow us to use new design elements from the Material library in our application.

--- apps/frontend/src/app/shared/material/material.module.ts
+++ apps/frontend/src/app/shared/material/material.module.ts
@@ -5,8 +5,11 @@ import { MatListModule, MatListOption, MatSelectionList } from '@angular/materia
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
-const material = [MatToolbarModule, MatSidenavModule, MatListModule, MatListOption, MatSelectionList, MatCardModule, MatButtonModule, MatGridListModule];
+const material = [MatToolbarModule, MatSidenavModule, MatListModule, MatListOption, MatSelectionList, MatCardModule, MatButtonModule, MatGridListModule, MatFormFieldModule, MatIconModule, MatInputModule];
 
 
 @NgModule({
