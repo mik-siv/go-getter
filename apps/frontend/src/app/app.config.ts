8d9cd7082b56399d4f9daf1874ecf95738c2e775
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-06-16 21:36:55 +0300

    Add HttpClientModule to app config
    A HttpClientModule was added to the application configuration file to ensure all HTTP communications are handled correctly in the application. This change helps to manage all HTTP requests through a single module.

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -14,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
     MaterialModule,
     importProvidersFrom(HttpClientModule),
+    HttpClientModule
   ],
 };
