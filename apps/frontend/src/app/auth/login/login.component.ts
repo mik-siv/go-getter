6ddea94f5b1789a57a5b86096aa19cdfd5e3e6ad
mikhail.sivinskikh <mikhail.sivinskikh@gmail.com>
2024-06-16 21:39:36 +0300

    feat: Implement user login functionality
    A user login component was extended with fields for email and password. It includes validations for both fields and functionality to redirect a user to the home page if users are already authenticated. Related styling changes were also applied.

--- apps/frontend/src/app/auth/login/login.component.ts
+++ apps/frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { MaterialModule } from '../../shared/material/material.module';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../shared/services/data-access/auth/auth.service';
+import { Router } from '@angular/router';
+
+export interface UserFormData {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup<UserFormData>;
+  fb: FormBuilder = inject(FormBuilder);
+  authService: AuthService = inject(AuthService);
+  router = inject(Router);
 
+  constructor() {
+    effect(() => {
+      if (this.authService.user()) {
+        this.router.navigate(['home']);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', { validators: [Validators.required, Validators.email] }],
+      password: ['', { validators: [Validators.required, Validators.minLength(8)] }],
+    });
+  }
+
+  login(): void {
+    const email: string = this.loginForm.value.email || '';
+    const password: string = this.loginForm.value.password || '';
+    this.authService.login(email, password).subscribe();
+  }
+
+  get isFormInvalid(): boolean{
+    return this.loginForm.invalid;
+  }
 }
+
